c8aa19af4f1f90e6c568f4f24b36ce7178324986
ojfernandess <odairjosfernandess@gmail.com>
2025-01-15 07:17:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ app.post("/api/profit/:planId", authenticate, (req, res) => {
 });
 
 // Inicia o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
